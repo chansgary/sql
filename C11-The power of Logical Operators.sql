18f254a4c2225ee17090bd0f5c54768565ea5709
chansgary <63241838+chansgary@users.noreply.github.com>
2021-07-20 17:44:37 +0800

    update notes of C11.

--- C11-The power of Logical Operators.sql
+++ C11-The power of Logical Operators.sql
@@ -191,3 +191,162 @@ select title, author_lname
 from books
 where author_lname in ('carver','lahiri','smith');
 
+select title,released_year
+from books
+where released_year in (2004,2005,2006);
+
+################################################################################
+# NOT IN
+# get all books that are not published in
+# 2000,2002,2004,2006,2008,2010,2012,2014,2016
+select title, released_year
+from books
+where released_year != 2002 AND
+      released_year != 2004 AND
+      released_year != 2006 AND
+      released_year != 2008 AND
+      released_year != 2010 AND
+      released_year != 2012 AND
+      released_year != 2014 AND
+      released_year != 2016;
+
+# Too verbose!!!
+
+# massive difference, much easier to add on
+select title,released_year
+from books
+where released_year > 2000 AND
+      released_year not in(2000,2002,2004,2006,2008,2010,2012,2014,2016);
+# %
+
+select title,released_year
+from books
+where released_year >= 2000 AND
+      released_year % 2 != 0;
+
+################################################################################
+# Case Statements
+
+
+# genre changing depending on the released_year
+select title,released_year,
+       case
+           when released_year >= 2000 then 'Modern lit'
+           else '20th Century Lit'
+       end as Genre
+from books;
+
+# * - low
+# ** - medium
+# *** hight
+
+select title,stock_quantity,
+       case
+           when stock_quantity between 0 and 50 then '*'
+           when stock_quantity between 51 and 100 then '**'
+           else '***'
+       end as stock
+from books;
+
+# No need of comma
+select title,
+       case
+           when stock_quantity between 0 and 50 then '*'
+           when stock_quantity between 51 and 100 then '**'
+           when stock_quantity between 101 and 150 then '***'
+           else '****'
+       end as STOCK
+from books;
+
+# more succinct version:
+# I think that from the very beginning we should set stock_quantity to be larger than or equal to 0.
+select title,
+       case
+           when stock_quantity <= 50 then '*'
+           when stock_quantity <= 100 then '**'
+           when stock_quantity <= 150 then '***'
+           else '****'
+       end as STOCK
+from books;
+
+################################################################################
+# Logical Operators Exercises
+
+# evaluate the following
+select 10 != 10;
+# guess(result): 0
+
+select 15 > 14 && 99 - 5 <= 94;
+# guess(result): 1
+
+select 1 in (5,3) || 9 between 8 AND 10;
+# guess(result): 1
+
+# select all books written before 1980(non inclusive)
+select * from books
+where released_year < 1980;
+
+# select all books written by Eggers or Chabon
+select title,author_lname
+from books
+where author_lname in ('Eggers','Chabon');
+
+select title,author_lname
+from books
+where author_lname = 'Eggers' OR
+      author_lname = 'Chabon';
+
+# select all books written by lahiri published after 2000
+select title, author_lname
+from books
+where author_lname = 'lahiri' AND
+      released_year > 2000;
+
+# select all books with page counts between 100 and 200
+select title,pages
+from books
+where pages between 100 and 200;
+
+select title,pages
+from books
+where pages >= 100 and pages <=200;
+
+# select all books where author_lname starts with a 'C' or an 'S'
+select title,author_lname from books
+where author_lname like 'C%' or
+      author_lname like 'S%';
+
+# if title contains 'stories', short stories
+# just kids and a heartbreaking work -> memoir
+# everything else -> novel
+# apparently need a case statement
+
+select title, author_lname,
+       case
+           when title like '%stories%' then 'Short Stories'
+           when title like 'just kids' or 'a hearting work' then 'Memoir'
+           else 'Novel'
+       end as Type
+from books;
+
+# title, author_lname, count: how many books each author has book/books
+
+
+# challenging problems
+# command: mysql> SET @@sql_mode='';
+select title,author_lname,
+    case
+        when count(*) = 1 then concat(count(*),' book')
+        else concat(count(*),' books')
+    end as COUNT
+from books
+group by author_fname,author_lname
+order by author_lname;
+
+# Burn all the books
+# drop table xxx;
+# drop database xxx;
+
+
+
+
